75580c7f3ece79e274d8036119ef65a7a253cc85
Masahiro Okubo <stmhamachiii@gmail.com>
2021-01-12 08:37:33 +0900

    不要なスペースの削除

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,10 @@
-import os, sys
+import os
+import sys
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -12,8 +12,6 @@ sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..'
 
 import importlib
 from sqlalchemy.schema import MetaData
-
-from app.database import Base
 
 config = context.config
 
